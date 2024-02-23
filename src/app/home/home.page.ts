abe125869ff142210a05d3175cb6a936d14552f5
JGSebastian <juan.gaiborc.est@uets.edu.ec>
2024-02-23 16:10:14 -0500

    SumativaPokedexSuprema
    Ya esta listisimo la pokedex solo falta lo del firebase y lo del arduino, pilas crak

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { APIServiceService } from '../Services/apiservice.service';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,40 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  pokemon: any
+  pokemonId: number=0;
+  valor: any;
+  pokemonName: string="";
+  pokemonType: string="";
+  constructor(private api: APIServiceService) {}
 
+
+  getPokemonData() {
+    try {
+      if (isNaN(this.valor)) {
+        // Si valor no es un número, se considera nombre
+        this.api.getPokemonName(this.valor).subscribe((response => {
+          this.pokemon = response;
+          console.log(this.pokemon)
+          console.log(this.pokemon.sprites.front_default)
+          for (let i = 0; i < this.pokemon.types.length; i++) {
+            console.log(this.pokemon.types[i].type.name);
+          }
+        }));
+      } else {
+        // Si valor es un número, se busca por ID
+        this.api.getPokemonID(this.valor).subscribe((response => {
+          this.pokemon = response;
+          console.log("Tipos de Pokémon:");
+          for (let i = 0; i < this.pokemon.types.length; i++) {
+            console.log(this.pokemon.types[i].type.name);
+          }
+        }));
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  
 }
