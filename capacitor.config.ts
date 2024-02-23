a18178d97d089df9d2bb7ec3343f35f8501a92f5
GOJoelDavid <145133849+GOJoelDavid@users.noreply.github.com>
2024-02-23 11:54:25 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'PokedeksFull',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
